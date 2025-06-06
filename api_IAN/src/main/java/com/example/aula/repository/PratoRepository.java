f55d8708a27d08c601f99f2c9eed64656645e4ea
ianzinho23 <159203880+ianzinho23@users.noreply.github.com>
2025-06-06 12:18:36 -0300

    Delete api_IAN directory

--- api_IAN/src/main/java/com/example/aula/repository/PratoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aula.repository;
-
-import com.example.aula.model.Prato;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PratoRepository extends JpaRepository<Prato, Long> {
-}
